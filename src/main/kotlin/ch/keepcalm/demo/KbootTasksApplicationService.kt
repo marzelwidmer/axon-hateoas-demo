fc7e908eb5ffb8f7ef1ca65d391fec940f0bffca
Marcel Widmer <marzelwidmer@gmail.com>
2022-04-04 13:44:32 +0200

    Configure Axon with MongoDB

--- src/main/kotlin/ch/keepcalm/demo/KbootTasksApplicationService.kt
+++ src/main/kotlin/ch/keepcalm/demo/KbootTasksApplicationService.kt
@@ -1,6 +1,8 @@
 package ch.keepcalm.demo
 
+import ch.keepcalm.demo.infrastructure.configuration.AxonSnapshotThresholdConfigurer
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.hateoas.config.EnableHypermediaSupport
@@ -9,12 +11,14 @@ import org.springframework.web.server.adapter.ForwardedHeaderTransformer
 
 @SpringBootApplication
 @EnableHypermediaSupport(stacks = [WebStack.WEBFLUX], type = [EnableHypermediaSupport.HypermediaType.HAL])
+@EnableConfigurationProperties(AxonSnapshotThresholdConfigurer::class)
 class KbootTasksApplicationService {
 
     @Bean
     fun forwardedHeaderTransformer(): ForwardedHeaderTransformer? {
         return ForwardedHeaderTransformer()
     }
+
 }
 
 fun main(args: Array<String>) {
