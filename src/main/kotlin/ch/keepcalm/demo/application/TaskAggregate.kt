b18385c5b3d1f35ff9a3080b8ff8a955e77be669
Marcel Widmer <marzelwidmer@gmail.com>
2022-04-04 08:55:22 +0200

    TDD Green CreateTask and TaskCreatedEvent

--- src/main/kotlin/ch/keepcalm/demo/application/TaskAggregate.kt
+++ src/main/kotlin/ch/keepcalm/demo/application/TaskAggregate.kt
@@ -1,5 +1,30 @@
 package ch.keepcalm.demo.application
 
+import TaskCreatedEvent
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.modelling.command.AggregateIdentifier
+import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.spring.stereotype.Aggregate
+
+@Aggregate
 class TaskAggregate {
+
+    @AggregateIdentifier
+    private lateinit var taskId: String
+
+    constructor() {}
+
+    @CommandHandler
+    constructor(command: CreateTaskCommand) {
+    	if (!this::taskId.isInitialized) {
+    		AggregateLifecycle.apply(TaskCreatedEvent(taskId = command.taskId, date = command.date))
+    	}
+    }
+
+    @EventSourcingHandler
+    fun on(event: TaskCreatedEvent){
+        taskId = event.taskId
+    }
 
 }
