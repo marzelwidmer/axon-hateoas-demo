25d9aeaa6c9039509c0e30ba6e9d9e9d0c62961e
Marcel Widmer <marzelwidmer@gmail.com>
2022-04-10 19:32:53 +0200

    test: task Done test expect TaskDoneEvent

--- src/test/kotlin/ch/keepcalm/demo/application/TaskAggregateTests.kt
+++ src/test/kotlin/ch/keepcalm/demo/application/TaskAggregateTests.kt
@@ -1,23 +1,20 @@
 package ch.keepcalm.demo.application
 
 import ch.keepcalm.demo.domain.TaskCreatedEvent
+import ch.keepcalm.demo.domain.TaskDoneEvent
 import ch.keepcalm.demo.domain.TaskId
 import ch.keepcalm.demo.domain.TaskState
 import org.axonframework.test.aggregate.AggregateTestFixture
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import java.time.LocalDateTime
 import java.util.*
 
 class TaskAggregateTests {
 
-    // Initialize aggregate test
-    private val fixture: AggregateTestFixture<TaskAggregate> by lazy {
-        AggregateTestFixture(TaskAggregate::class.java)
-    }
-
-
     @Test
-    fun `Create task command will expect a TaskCreatedEvent`() {
+    fun `create a new task will expect a TaskCreatedEvent`() {
+        val fixture = AggregateTestFixture(TaskAggregate::class.java)
         val now = LocalDateTime.now()
         val validValue = UUID.randomUUID().toString()
         val taskId = TaskId(validValue)
@@ -26,4 +23,17 @@ class TaskAggregateTests {
             .expectEvents(TaskCreatedEvent(taskId = taskId, date = now, state = TaskState.Open))
     }
 
+    @Test
+    fun `given a TaskCreatedEvent set task to DONE  will expect a TaskDoneEvent` () {
+        val fixture = AggregateTestFixture(TaskAggregate::class.java)
+        val now = LocalDateTime.now()
+        val validValue = UUID.randomUUID().toString()
+        val taskId = TaskId(validValue)
+        fixture
+            .given(TaskCreatedEvent(taskId = taskId, date = now, state = TaskState.Open))
+            .`when`(DoneTaskCommand(taskId = taskId, date = now))
+            .expectEvents(TaskDoneEvent(
+                taskId = taskId, date =now, state =TaskState.Done))
+
+    }
 }
